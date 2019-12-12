a85cccff355c4b96a4a7195f0f084e31ed48021f
Daniel Macauyag <dmacauyag92@gmail.com>
2019-12-12 14:46:51 -0800

    adds method for inserting node at the end of the list

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ class LinkedList {
   }
 }
 
+function createNewNode (data) {
+  return new Node(data)
+}
+
 LinkedList.prototype.insertAtBeginning = function (data) {
-  const newNode = new Node(data)
+  const newNode = createNewNode(data)
   newNode.next = this.head
 
   this.head = newNode
+  return this.head
+}
+
+LinkedList.prototype.insertAtEnd = function (data) {
+  const newNode = createNewNode(data)
+
+  if (!this.head) {
+    this.head = newNode
+    return this.head
+  }
+
+  // Traverse the list to find the tail node
+  let tail = this.head
+  while (tail.next !== null) tail = tail.next
+  tail.next = newNode
+
   return this.head
 }
